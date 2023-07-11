3e32e1ccd1cae990424d56d4097834767c8851ea
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-07-11 09:29:27 -0300

    fiz algumas alterações

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 78.2
+var vel = 50
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
-if (vel >= 60) {
+if (vel > 60) {
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
 
