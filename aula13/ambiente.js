cdbe598fab7e9d9acf37b4cde141487dd6297ee1
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-07-12 13:05:50 -0300

    testando estruturas de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while  (c <= 100)
+
+// var number = 1
+// while (number <= 10) {
+//     console.log(`Passo ${number}`)
+//     number++
+// }
