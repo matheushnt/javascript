bf13fe58eb6a4caefd2ce9db6cd9ea249ac38e63
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-07-12 14:19:42 -0300

    exercício de estrutura de repetição FOR

---
+++ aula14/ambiente.js
@@ -0,0 +1,15 @@
+// var a = 1
+// while (a <= 10) {
+//     console.log(`Passo ${a}`)
+//     a ++
+// }
+
+// var b = 0
+// do {
+//     console.log(`Passo ${b}`)
+//     b += 2 
+// } while (b <= 20)
+
+for (c = 1; c <= 30; c++) {
+    console.log(`Passo ${c}`)
+}
